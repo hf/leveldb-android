81067d9eac52cc008b94fe793391e19e223d60ac
Stojan Dimitrovski <sdimitrovski@gmail.com>
2016-10-15 22:47:17 +0200

    Add 64 bit Android ABIs, and MIPS ABIs

--- leveldb/src/main/jni-prebuild/Application.mk
+++ leveldb/src/main/jni-prebuild/Application.mk
@@ -1,5 +1,5 @@
 APP_PLATFORM := android-8
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64 mips mips64
 APP_STL := gnustl_static
 LOCAL_CPPFLAGS := -std=c++11 -fexceptions
 LOCAL_CPP_EXTENSION = .cc
